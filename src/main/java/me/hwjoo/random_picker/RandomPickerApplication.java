47c15223f6bae47579f45e1b025a05d2a0d0596a
hwjoo <jhw664089@gmail.com>
2025-02-26 13:06:05 +0900

    init project

---
+++ src/main/java/me/hwjoo/random_picker/RandomPickerApplication.java
@@ -0,0 +1,13 @@
+package me.hwjoo.random_picker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomPickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hwjoo/random_picker/RandomPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hwjoo.random_picker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
